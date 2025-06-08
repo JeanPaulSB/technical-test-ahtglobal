dd32eb172d2772aa51d1c2d8bc4545310d8b940a
JeanPaulSB <jeanpaulsierraboom@gmail.com>
2025-06-07 21:53:18 -0500

    fix: modify depends on and create logic for edit view

--- app/inventory.py
+++ app/inventory.py
@@ -1,7 +1,8 @@
+import logging
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from .database import db_session
 from .models import Product
-import logging
+
 
 bp = Blueprint("inventory", __name__)
 logging.basicConfig(level=logging.DEBUG)
@@ -14,15 +15,17 @@ def home():
 
 
 @bp.route("/add", methods=["GET", "POST"])
-def create():
+def add():
     if request.method == "POST":
         name = request.form["name"]
+        price = request.form["price"]
         mac_address = request.form["mac_address"]
         serial_number = request.form["serial_number"]
         manufacturer = request.form["manufacturer"]
         description = request.form["description"]
         product = Product(
             name=name,
+            price=price,
             mac_address=mac_address,
             serial_number=serial_number,
             manufacturer=manufacturer,
@@ -52,4 +55,13 @@ def delete(id):
 @bp.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit(id):
     product = Product.query.get(id)
+    if request.method == "POST":
+        product.name = request.form["name"]
+        product.price = request.form["price"]
+        product.mac_address = request.form["mac_address"]
+        product.serial_number = request.form["serial_number"]
+        product.manufacturer = request.form["manufacturer"]
+        product.description = request.form["description"]
+        db_session.commit()
+        return redirect(url_for("inventory.home"))
     return render_template("crud/edit.html", product=product)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text, Float
 from .database import Base
 
 
@@ -6,6 +6,7 @@ class Product(Base):
     __tablename__ = "inventory"
     id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
+    price = Column(Float)
     mac_address = Column(String(100), unique=True)
     serial_number = Column(String(100), unique=True)
     manufacturer = Column(String(100))
@@ -14,12 +15,14 @@ class Product(Base):
     def __init__(
         self,
         name=None,
+        price=price,
         mac_address=None,
         serial_number=None,
         manufacturer=None,
         description=None,
     ):
         self.name = name
+        self.price = price
         self.mac_address = mac_address
         self.serial_number = serial_number
         self.manufacturer = manufacturer

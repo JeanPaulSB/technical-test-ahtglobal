175d1342087460b5937148073b9bab97e398f8b7
JeanPaulSB <jeanpaulsierraboom@gmail.com>
2025-06-07 20:40:51 -0500

    feat: update templates and create routes for each action

--- app/__init__.py
+++ app/__init__.py
@@ -4,8 +4,6 @@ from flask import Flask, render_template, redirect, url_for, request
 from .database import init_db, db_session
 from .models import Product
 
-logging.basicConfig(level=logging.DEBUG)
-
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -13,9 +11,9 @@ def create_app(test_config=None):
     init_db()
 
     from . import inventory
-    app.register_blueprint(inventory.bp)
-    app.add_url_rule('/', endpoint='index')
 
+    app.register_blueprint(inventory.bp)
+    app.add_url_rule("/", endpoint="index")
 
     @app.teardown_appcontext
     def shutdown_session(exception=None):
--- app/inventory.py
+++ app/inventory.py
@@ -1,31 +1,55 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
-)
+from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from .database import db_session
 from .models import Product
+import logging
 
-bp = Blueprint('inventory', __name__)
+bp = Blueprint("inventory", __name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def home():
     products = Product.query.all()
-    return render_template("crud/list.html",products = products)
+    return render_template("crud/list.html", products=products)
 
-@bp.route("/add")
+
+@bp.route("/add", methods=["GET", "POST"])
 def create():
+    if request.method == "POST":
+        name = request.form["name"]
+        mac_address = request.form["mac_address"]
+        serial_number = request.form["serial_number"]
+        manufacturer = request.form["manufacturer"]
+        description = request.form["description"]
+        product = Product(
+            name=name,
+            mac_address=mac_address,
+            serial_number=serial_number,
+            manufacturer=manufacturer,
+            description=description,
+        )
+        db_session.add(product)
+        db_session.commit()
+        return redirect(url_for("inventory.home"))
     return render_template("crud/create.html")
-    
-@bp.route("/add-request",methods = ['POST'])
-def add_request():
-    name = request.form["name"]
-    mac_address = request.form["mac_address"]
-    serial_number = request.form["serial_number"]
-    manufacturer = request.form["manufacturer"]
-    description = request.form["description"]
 
-    product = Product(name=name, mac_address=mac_address,serial_number=serial_number,manufacturer=manufacturer,description=description)
-    db_session.add(product)
-    db_session.commit()
 
-    return redirect(url_for('home'))
+@bp.route("/delete/<int:id>", methods=["GET", "POST"])
+def delete(id):
+    if request.method == "POST":
+        id = request.form["id"]
+        product = Product.query.get(id)
+        logging.info(product)
+        if product:
+            db_session.delete(product)
+            db_session.commit()
+            return redirect(url_for("inventory.home"))
+
+    product = Product.query.get(id)
+    return render_template("crud/delete.html", product=product)
+
+
+@bp.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id):
+    product = Product.query.get(id)
+    return render_template("crud/edit.html", product=product)
